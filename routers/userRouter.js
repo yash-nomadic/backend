ab222b6532801166dc667643a5e7f94dfa9caa54
yash-nomadic <yashsingh4537@gmail.com>
2025-02-15 13:56:04 +0530

    userRouter backend

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,20 +5,46 @@ const router = express.Router();
 
 router.post('/add', (req, res) => {
     console.log(req.body);
-    
+
     new Model(req.body).save()
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        res.status(500).json(err);
-    });
-    
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.status(500).json(err);
+        });
+
 });
 
 //getall
 router.get('/getall', (req, res) => {
-    res.send('response from user getall')
+
+    Model.find()
+        .then((result) => {
+            res.status(200).json(result);
+
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+
+        });
+
+
 });
+
+// : denotes url parameter
+router.get('/getbycity/:city', (req, res) => {
+    console.log(req.params.city);
+    Model.find({ city: req.params.city })
+        .then((result) => {
+            res.status(200).json(result);
+
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+
+        });
+
+})
 
 //getbyid
 router.get('/getbyid', (req, res) => {
