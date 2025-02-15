e5dd780f468b9d804bf799141cc217d7cc3df318
yash-nomadic <yashsingh4537@gmail.com>
2025-02-15 13:55:50 +0530

    userModels backend

--- models/userModel.js
+++ models/userModel.js
@@ -4,14 +4,9 @@ const mySchema = new Schema({
     name: String,
     email: { type: String, unique: true },
     password: { type: String, require: true },
+    city: { type: String, default: 'unknown' },
     createdAt: { type: Date, default: Date.now }
-
+    
 });
 
 module.exports = model('users', mySchema);
-
-// title : String
-// price : Number
-// description : String
-// category : String
-// image : String
