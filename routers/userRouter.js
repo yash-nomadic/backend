bdd6453e859ae260f6b13bef4a21a93bae89b275
yash-nomadic <yashsingh4537@gmail.com>
2025-02-16 17:15:44 +0530

    user Router

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -44,21 +44,59 @@ router.get('/getbycity/:city', (req, res) => {
 
         });
 
+});
+
+// email
+router.get('/getbyemail/:email', (req, res) => {
+    console.log(req.params.email);
+    Model.findOne({ email: req.params.email })
+        .then((result) => {
+            res.status(200).json(result);
+
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+
+        });
+
 })
 
+
 //getbyid
-router.get('/getbyid', (req, res) => {
-    res.send('response from user getbyid')
+router.get('/getbyid/:id', (req, res) => {
+    Model.findById(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 //update
-router.get('/update', (req, res) => {
-    res.send('response from user update')
+router.put('/update/:id', (req, res) => {
+    
+    Model.findByIdAndUpdate(req.params.id, req.body, {new : true})
+
+    .then((result) => {
+        res.status(200).json(result);
+               
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err); 
+    });
+
 });
 
 //delete
-router.get('/delete', (req, res) => {
-    res.send('response from user delete')
+router.delete('/delete/:id', (req, res) => {
+    Model.findByIdAndDelete(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
