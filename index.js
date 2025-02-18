741fca3b9423de2a93975939d8356017b4d1ca14
yash-nomadic <yashsingh4537@gmail.com>
2025-02-18 22:11:55 +0530

    index .js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = 5000;
 //middleware
 app.use(cors({ origin: ['http://localhost:3000'] }));
 app.use(express.json());
-app.use('/user', userRouter);
+app.use('/user', userRouter);   
 app.use('/product', productRouter);
  
 
